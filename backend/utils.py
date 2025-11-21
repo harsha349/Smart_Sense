88cdd6f705ae80f5b35c98b3cbc32ef28ef313b5
Harsha,kumari <harshakumari11982@gmail.com>
2025-11-21 03:03:27 +0000

    util changes

--- backend/utils.py
+++ backend/utils.py
@@ -1,2 +1,11 @@
-def read_image_bytes(file_storage):
-    return file_storage.read()
+# def read_image_bytes(file_storage):
+#     return file_storage.read()
+import base64
+
+def read_image_bytes(file):
+    if not file:
+        return None
+    img_bytes = file.read()
+    return base64.b64encode(img_bytes).decode('utf-8')
+
+
